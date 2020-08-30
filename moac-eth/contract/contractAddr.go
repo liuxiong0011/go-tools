cac9fb64c09bf30b1b85f978f4d742af0a50ecc1
Shawn sh desktop-server <liuxmzc@163.com>
2020-08-31 01:25:33 +0800

    update contractAddr.go

--- moac-eth/contract/contractAddr.go
+++ moac-eth/contract/contractAddr.go
@@ -8,9 +8,25 @@ import (
 )
 
 func main() {
-	account := common.HexToAddress("0xcB8775C1726c797a1855E86b9F22A7fa0CB7532D") //0xc12031318143c148aedea573e1b0f691e33f3092
-	addr := getCreateAddress(account, 1)
-	fmt.Printf("ContractAddress: %s\n", addr.String())
+	// 0xc12031318143c148aedea573e1b0f691e33f3092
+	account1 := common.HexToAddress("0xc12031318143c148aedea573e1b0f691e33f3092")
+	account2 := common.HexToAddress("0xc12031318143c148aedea573e1b0f691e33f3093")
+	account3 := common.HexToAddress("0xc12031318143c148aedea573e1b0f691e33f3094")
+
+	for i := 0; i < 3; i++ {
+		addr := getCreateAddress(account1, uint64(i))
+		fmt.Printf("main--%d--[%s]: %s\n", i, account1.String(), addr.String())
+	}
+	fmt.Println("")
+	for i := 3; i < 8; i++ {
+		addr := getCreateAddress(account2, uint64(i))
+		fmt.Printf("fund--%d--[%s]: %s\n", i, account2.String(), addr.String())
+	}
+	fmt.Println("")
+	for i := 0; i < 5; i++ {
+		addr := getCreateAddress(account3, uint64(i))
+		fmt.Printf("fund--%d--[%s]: %s\n", i, account3.String(), addr.String())
+	}
 }
 
 func getCreateAddress(b common.Address, nonce uint64) common.Address {
